e11e1d1f880166950296f6f85a7b1200a5627f4c
isak4307 <isak4307@gmail.com>
2025-11-21 22:54:25 +0100

    fixed application code

--- code/FeedAppApplication.java
+++ code/FeedAppApplication.java
@@ -1,2 +1,12 @@
+package dat250.feedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FeedAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedAppApplication.class, args);
+    }
 }
